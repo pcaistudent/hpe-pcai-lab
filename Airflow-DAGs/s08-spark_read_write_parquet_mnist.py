f3311cf6f65f990b69e70f965add3af4dd894022
pcaistudent <pcaistudent@gmail.com>
2025-04-26 12:04:07 -0400

    Update s08-spark_read_write_parquet_mnist.py

--- Airflow-DAGs/s08-spark_read_write_parquet_mnist.py
+++ Airflow-DAGs/s08-spark_read_write_parquet_mnist.py
@@ -54,7 +54,7 @@ dag = DAG(
         ),
     },
     render_template_as_native_obj=True,
-    access_control={"Admin": {"can_read"},"student1032.08-hpelabsonline.com": {"can_read","can_edit","can_delete"}},
+    access_control={"student1032.08-hpelabsonline.com": {"can_read","can_edit","can_delete"}},
 )
 
 submit = SparkKubernetesOperator(
