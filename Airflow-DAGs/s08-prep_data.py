f13b351726beabaf2f49f73266fc80747d49af35
pcaistudent <pcaistudent@gmail.com>
2025-05-05 19:17:50 -0400

    Add files via upload

--- Airflow-DAGs/s08-prep_data.py
+++ Airflow-DAGs/s08-prep_data.py
@@ -74,19 +74,17 @@ submit = SparkKubernetesOperator(
     dag=dag,
     enable_impersonation_from_ldap_user=True,
 )
-userVolMount = k8s.V1VolumeMount(
-    name="user-volume", mount_path="/mnt/user", sub_path=None, read_only=False
-    )
-userVol = k8s.V1Volume(
-    name="user-volume",
-    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="user-pvc"),
-    )
 datamove = KubernetesPodOperator(
     task_id="data-move",
     image="gcr.io/mapr-252711/kubeflow/notebooks/jupyter-scipy:ezua-1.6.1-b32915fa",
     cmds=["python","/mnt/user/Airflow/data-move.py]}}","/mnt/user/{{dag_run.conf['export_path']}}","/mnt/user/{{dag_run.conf['export_path_2']}}"],
-    volumes = [userVol],
-    volume_mounts = [userVolMount],
+    volume_mounts = [k8s.V1VolumeMount(
+    name="user-volume", mount_path="/mnt/user", read_only=False
+    )],
+    volumes = [k8s.V1Volume(
+    name="user-volume",
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="user-pvc"),
+    )],
     image_pull_secrets = "hpe-imagepull-secrets",
     dag=dag,
 )
