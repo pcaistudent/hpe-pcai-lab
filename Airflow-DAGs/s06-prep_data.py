faf41ed43dfc7e574742fcec90ce5e9c2db61e3e
pcaistudent <pcaistudent@gmail.com>
2026-04-29 18:53:38 -0400

    Add files via upload

--- Airflow-DAGs/s06-prep_data.py
+++ Airflow-DAGs/s06-prep_data.py
@@ -9,7 +9,7 @@ from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import (
 from airflow.providers.cncf.kubernetes.operators.pod import (
     KubernetesPodOperator,
 )
-from airflow.utils.dates import days_ago
+from airflow.utils.dates import days_ago(1)
 from kubernetes.client import models as k8s
 
 default_args = {
